2bb2f7bb48e6eb072d6396a77283ff8b0f3603d0
Tanishq-sahu31 <tanishq.31.sahu@gmail.com>
2026-01-08 09:19:33 +0530

    Create leetcode167.cpp

---
+++ leetcode167.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        int i=0;
+        int j=numbers.size()-1;
+        while(i<=j){
+
+                if(target==numbers[i]+numbers[j])return {i+1,j+1};
+                if(target>numbers[i]+numbers[j]){
+                    i++;
+
+                }
+                if(target<numbers[i]+numbers[j]){
+                    j--;
+
+                }
+
+        }
+        return {};
+    }
+};
