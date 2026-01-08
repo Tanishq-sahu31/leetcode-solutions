431fcc7f11bb64d613f90e462ac43aed3ac0a4cc
Tanishq-sahu31 <tanishq.31.sahu@gmail.com>
2026-01-08 18:11:27 +0530

    Create lc11.cpp

---
+++ lc11.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        int maxWater = 0;
+
+        while (i < j) {
+            int area = (j - i) * min(height[i], height[j]);
+            maxWater = max(maxWater, area);
+
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxWater;
+    }
+};
